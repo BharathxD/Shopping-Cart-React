ba46540c750e5d83d47fbb2b7c39a412f9a3fa84
Bharath Lakshman Kumar <bharath@Bharaths-MacBook-Air.local>
2022-09-08 12:33:54 +0530

    styled elements

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import CartItem from './Cartitem';
+
+function App() {
+  return (
+    <div className="App">
+      <CartItem />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Cartitem.js
@@ -0,0 +1,45 @@
+import React from 'react';
+
+class CartItem extends React.Component {
+    constructor() {
+        super(); // Calling the constructor first
+        this.state = { // Essential Data
+            price : 999,
+            title: 'Phone',
+            qty: 1,
+            img: ''
+        } 
+    }
+    render () {
+        const { title, price, qty } = this.state;
+        return (
+            <div className="cart-item">
+            <div className="left-block">
+            <img style={styles.image}/>
+            </div>
+            <div className="right-block">
+            <div style={ { fontSize: 25 } } > {title} </div>
+            <div style={ { color: '#777' } } > {price} </div>
+            <div style={ { color: '#777' } } > {qty} </div>
+            <div className="cart-item-actions">
+            {/* Buttons */}
+            <img alt="increase" className="action-icons" src="https://cdn-icons-png.flaticon.com/512/992/992651.png"/>
+            <img alt="decrease" className="action-icons" src="https://cdn-icons-png.flaticon.com/512/992/992683.png" />
+            <img alt="delete" className="action-icons"  src="https://cdn-icons-png.flaticon.com/512/1450/1450571.png"/>
+            </div>
+            </div>
+            </div>
+        );
+    }
+}
+
+const styles = {
+    image: {
+        height: 110,
+        width: 110,
+        borderRadius: 4,
+        background: '#ccc'
+    }
+}
+
+export default CartItem;
